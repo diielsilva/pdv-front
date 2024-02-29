349893ede0c847df5ad5a48b30ddc22063d51e1f
Daniel Silva <diielsilvasousa@gmail.com>
2024-02-29 15:31:33 -0300

    refactor: refactoring search sale component

--- src/app/pages/sales/search/search-sale/search-sale.component.ts
+++ src/app/pages/sales/search/search-sale/search-sale.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
@@ -7,6 +7,7 @@ import { PanelModule } from 'primeng/panel';
 import { SaleComponent } from '../../../../common/components/sale/sale.component';
 import { LoadingHelper } from '../../../../common/helpers/loading.helper';
 import { MessageHelper } from '../../../../common/helpers/message.helper';
+import { SubscriptionHelper } from '../../../../common/helpers/subscription.helper';
 import { Sale } from '../../../../core/models/sale';
 import { ReportService } from '../../../../core/services/report.service';
 import { SaleService } from '../../../../core/services/sale.service';
@@ -18,56 +19,57 @@ import { SaleService } from '../../../../core/services/sale.service';
   templateUrl: './search-sale.component.html',
   styleUrl: './search-sale.component.css'
 })
-export class SearchSaleComponent implements OnInit {
+export class SearchSaleComponent implements OnInit, OnDestroy {
+  protected saleService = inject(SaleService)
   protected reportService = inject(ReportService)
+  protected subscriber = inject(SubscriptionHelper)
+  protected loader = inject(LoadingHelper)
+  protected messager = inject(MessageHelper)
   protected searchForm!: FormGroup
   protected searchedDate!: Date
-  protected loadingHelper = inject(LoadingHelper)
-  protected messageHelper = inject(MessageHelper)
-  protected saleService = inject(SaleService)
   protected sales: Sale[] = []
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.searchForm = new FormGroup({
       date: new FormControl<Date | null>(null, [Validators.required])
     })
   }
 
-  handleSearchSaleButton(): void {
-    if (this.searchForm.valid) {
-      this.searchedDate = this.searchForm.controls['date'].value
-      this.saleService.findActiveByDate(this.searchedDate).subscribe({
-        next: (sales: Sale[]) => {
-          if (sales.length === 0) {
-            this.messageHelper.displayMessage('Não foram encontradas vendas com a data selecionada', 'error')
-          } else {
-            this.sales = sales
-          }
-        },
-        error: (response: HttpErrorResponse) => {
-          this.messageHelper.displayMessage(response.error.message, 'error')
-        }
-      })
-    }
+  public ngOnDestroy(): void {
+    this.subscriber.clean()
   }
 
-  profitReport(): void {
-    this.reportService.generateReportByDate(this.searchedDate).subscribe({
+  protected searchSales(): void {
+    this.searchedDate = this.searchForm.controls['date'].value
+
+    const subscription = this.saleService.findActiveByDate(this.searchedDate).subscribe({
+      next: (response: Sale[]) => {
+        if (response.length === 0) {
+          this.messager.displayMessage('Não foram encontradas vendas com a data selecionada', 'error')
+        } else {
+          this.sales = response
+        }
+      },
+      error: (response: HttpErrorResponse) => {
+        this.messager.displayMessage(response.error.message, 'error')
+      }
+    })
+
+    this.subscriber.add(subscription)
+  }
+
+  protected profitReport(): void {
+    const subscription = this.reportService.generateReportByDate(this.searchedDate).subscribe({
       next: (response: Blob) => {
         const reportWindow = window.URL.createObjectURL(response)
         window.open(reportWindow)
       },
       error: (response: HttpErrorResponse) => {
-        this.messageHelper.displayMessage(response.error.message, 'error')
+        this.messager.displayMessage(response.error.message, 'error')
       }
     })
+
+    this.subscriber.add(subscription)
   }
 
-  shouldDisableSearchSaleButton(): boolean {
-    return this.loadingHelper.isUnderLoading() || this.searchForm.invalid
-  }
-
-  shouldDisableProfitReportButton(): boolean {
-    return this.loadingHelper.isUnderLoading()
-  }
 }
