6ea9bbfc73c3a5980fc272525a45d6591c376f6e
Daniel Silva <diielsilvasousa@gmail.com>
2024-04-15 09:47:25 -0300

    refactor: improving login component

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,18 +1,17 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
 import { PanelModule } from "primeng/panel";
 import { PasswordModule } from "primeng/password";
+import { take } from 'rxjs';
 import { LoadingComponent } from '../../../common/components/loading/loading.component';
 import { MessageComponent } from '../../../common/components/message/message.component';
 import { LoginRequest } from '../../../common/dtos/login/login.request';
 import { LoginResponse } from '../../../common/dtos/login/login.response';
 import { LoadingHelper } from '../../../common/helpers/loading.helper';
 import { MessageHelper } from '../../../common/helpers/message.helper';
-import { SubscriptionHelper } from '../../../common/helpers/subscription.helper';
 import { SecurityService } from '../../../core/services/security.service';
 
 @Component({
@@ -22,39 +21,34 @@ import { SecurityService } from '../../../core/services/security.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  protected securityService = inject(SecurityService);
-  protected router = inject(Router)
-  protected subscriber = inject(SubscriptionHelper)
-  protected loader = inject(LoadingHelper)
-  protected messager = inject(MessageHelper)
-  protected loginForm!: FormGroup
+export class LoginComponent implements OnInit {
+  protected form!: FormGroup;
+
+  public constructor(
+    protected securityService: SecurityService,
+    protected router: Router,
+    protected loadingHelper: LoadingHelper,
+    protected messageHelper: MessageHelper
+  ) { }
 
   public ngOnInit(): void {
-    this.loginForm = new FormGroup({
+    this.form = new FormGroup({
       login: new FormControl<string | null>(null, { validators: Validators.required }),
       password: new FormControl<string | null>(null, { validators: Validators.required })
     })
 
-    this.securityService.logout()
+    this.securityService.logout();
   }
 
-  public ngOnDestroy(): void {
-    this.subscriber.clean()
-  }
+  protected login(): void {
+    const dto: LoginRequest = { login: this.form.controls['login'].value, password: this.form.controls['password'].value };
 
-  protected attemptAuthenticate(): void {
-    const dto: LoginRequest = { login: this.loginForm.controls['login'].value, password: this.loginForm.controls['password'].value }
-
-    const subscription = this.securityService.login(dto)
-      .subscribe({
-        next: (response: LoginResponse) => {
-          this.securityService.initSession(response)
-          this.router.navigate(['/pdv'])
-        }
-      });
-
-    this.subscriber.add(subscription)
+    this.securityService.login(dto).pipe(take(1)).subscribe({
+      next: (response: LoginResponse) => {
+        this.securityService.initSession(response);
+        this.router.navigate(['/pdv']);
+      }
+    });
   }
 
 }
