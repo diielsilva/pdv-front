859b2d989a2506e4bb76b1d277e22681f7f330c6
Daniel Silva <diielsilvasousa@gmail.com>
2024-04-15 10:01:11 -0300

    refactor: improving insert product component code

--- src/app/pages/products/insert/insert-product.component.ts
+++ src/app/pages/products/insert/insert-product.component.ts
@@ -1,14 +1,13 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
+import { take } from 'rxjs';
 import { ProductRequest } from '../../../common/dtos/products/product.request';
 import { LoadingHelper } from '../../../common/helpers/loading.helper';
 import { MessageHelper } from '../../../common/helpers/message.helper';
-import { SubscriptionHelper } from '../../../common/helpers/subscription.helper';
 import { ProductService } from '../../../core/services/product.service';
 
 @Component({
@@ -18,42 +17,36 @@ import { ProductService } from '../../../core/services/product.service';
   templateUrl: './insert-product.component.html',
   styleUrl: './insert-product.component.css'
 })
-export class InsertProductComponent implements OnInit, OnDestroy {
-  protected productService = inject(ProductService)
-  protected subscriber = inject(SubscriptionHelper)
-  protected loader = inject(LoadingHelper)
-  protected messager = inject(MessageHelper)
-  protected insertForm!: FormGroup
+export class InsertProductComponent implements OnInit {
+  protected form!: FormGroup;
+
+  public constructor(
+    protected productService: ProductService,
+    protected loadingHelper: LoadingHelper,
+    protected messageHelper: MessageHelper
+  ) { }
 
   public ngOnInit(): void {
-    this.insertForm = new FormGroup({
+    this.form = new FormGroup({
       description: new FormControl<string | null>(null, { validators: [Validators.required] }),
       amount: new FormControl<number | null>(null, { validators: [Validators.required, Validators.min(0)] }),
       price: new FormControl<number | null>(null, { validators: [Validators.required, Validators.min(0)] })
-    })
+    });
   }
 
-  public ngOnDestroy(): void {
-    this.subscriber.clean()
-  }
-
-  public insertProduct(): void {
+  public save(): void {
     const dto: ProductRequest = {
-      description: this.insertForm.controls['description'].value,
-      amount: this.insertForm.controls['amount'].value,
-      price: this.insertForm.controls['price'].value
-    }
+      description: this.form.controls['description'].value,
+      amount: this.form.controls['amount'].value,
+      price: this.form.controls['price'].value
+    };
 
-    const subscription = this.productService.save(dto)
-      .subscribe({
-        next: () => {
-          this.messager.displayMessage('Produto cadastrado com sucesso!', 'success')
-          this.insertForm.reset()
-        },
-        error: (response: HttpErrorResponse) => this.messager.displayMessage(response.error.message, 'error')
-      })
-
-    this.subscriber.add(subscription)
+    this.productService.save(dto).pipe(take(1)).subscribe({
+      next: () => {
+        this.messageHelper.displayMessage('Produto cadastrado com sucesso!', 'success');
+        this.form.reset();
+      },
+    });
   }
 
 }
